a3ea2c62490c784baa9c7f941773016a2d02f14a
GodwinDogbey <godwindogbey95@gmail.com>
2024-02-27 22:56:56 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,11 +7,12 @@ const countStudents = (path) => {
   if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
-  const lines = fs.readFileSync(path, 'utf-8').toString()
+  const lines = fs.readFileSync(path, 'utf-8').toString('utf-8')
     .trim().split('\n');
   const groups = {};
   const fields = lines[0].split(',');
   const students = fields.slice(0, fields.length - 1);
+
   for (const line of lines.slice(1)) {
     const record = line.split(',');
     const value = record.slice(0, record.length - 1);
@@ -22,6 +23,7 @@ const countStudents = (path) => {
     const entries = students.map((name, idx) => [name, value[idx]]);
     groups[field].push(Object.fromEntries(entries));
   }
+
   const totals = Object.values(groups)
     .reduce((prev, cur) => (prev || []).length + cur.length);
   console.log(`Number of students: ${totals}`);
@@ -32,4 +34,3 @@ const countStudents = (path) => {
 };
 
 module.exports = countStudents;
-
